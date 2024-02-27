b845f0bdb91fa15a7a9460d6e4cfee20684e1157
Juliusz Gorzen <juliusz.gorzen@transporeon.com>
2024-02-27 11:03:45 +0100

    Add pages files and handle home page routing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,15 @@
-from st_pages import Page, show_pages, add_page_title
-
-add_page_title()
+import streamlit as st
+from st_pages import Page, show_pages
 
 show_pages(
     [
-        Page("home.py", "🏠 Start here"),
-        Page("one_shot_prompting.py", "1️⃣🔫 One-shot Prompting"),
-        Page("few_shot_prompting.py", "🎲🔫 Few-shot Prompting"),
-        Page("ner_one_shot_prompting.py", "📑➕1️🔫 NER + One-shot Prompting"),
-        Page("ner_few_shot_prompting.py", "📑➕🎲🔫 NER + Few-shot Prompting"),
-        Page("rag.py", "️⚒️ RAG")
+        Page("pages/home.py", "🏠 Start here"),
+        Page("pages/one_shot_prompting.py", "1️🔫 One-shot Prompting"),
+        Page("pages/few_shot_prompting.py", "🎲🔫 Few-shot Prompting"),
+        Page("pages/ner_one_shot_prompting.py", "📑➕1️🔫 NER + One-shot Prompting"),
+        Page("pages/ner_few_shot_prompting.py", "📑➕🎲🔫 NER + Few-shot Prompting"),
+        Page("pages/rag.py", "️⚒️ RAG")
     ]
 )
+
+st.switch_page("pages/home.py")
