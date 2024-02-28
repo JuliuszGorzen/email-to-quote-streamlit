094e9fb2ae892e0ba87a6457c645b4ac20b091be
Juliusz Gorzen <juliusz.gorzen@transporeon.com>
2024-02-28 13:35:51 +0100

    Add help icon to some fields with explanations of the fields and add warning to one of the field + submit button takes whole container width

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if st.session_state["authentication_status"]:
     st.markdown(DISPLAY_SIDEBAR_HTML, unsafe_allow_html=True)
 
     # Add a logout button
-    authenticator.logout(location="sidebar")
+    authenticator.logout(button_name="Leave AI world 😥💔", location="sidebar")
 
     # ---- MAIN PAGE ----
     st.title("Email to Quote - Streamlit App 📧➡️💰")
@@ -79,38 +79,38 @@ if st.session_state["authentication_status"]:
     with few_shot_prompting_tab:
         st.header("Few-shot Prompting 3️⃣🔫")
         st.markdown("""
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
-                sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                """)
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        """)
 
     with ner_zero_shot_prompting:
         st.header("NER + Zero-shot Prompting ✍️➕0️⃣🔫")
         st.markdown("""
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
-                sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                """)
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        """)
 
     with ner_few_shot_prompting:
         st.header("NER + Few-shot Prompting ✍️➕3️⃣🔫")
         st.markdown("""
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
-                sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                """)
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        """)
 
     with rag:
         st.header("RAG 📄")
         st.markdown("""
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
-                sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                """)
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        """)
 
     # ---- SIDEBAR ----
     st.sidebar.header("Model parameters:")
@@ -120,21 +120,64 @@ if st.session_state["authentication_status"]:
         st.write("Some parameters are disabled as for now we have only one LLM.")
 
         # Form fields
-        st.text_input(label="Azure endpoint", value="https://open-ai-resource-gen-ai.openai.azure.com/", disabled=True)
-        st.text_input(label="OpenAI API version", value="2023-07-01-preview", disabled=True)
-        st.text_input(label="OpenAI API key", value="******", disabled=True)
-        st.text_input(label="OpenAI API type", value="azure", disabled=True)
-        st.text_input(label="Deployment name", value="gpt-35-dev", disabled=True)
-        st.text_input(label="Model name", value="gpt-35-turbo", disabled=True)
-        st.text_input(label="Model version", value="0613", disabled=True)
+        azure_endpoint_val = st.text_input(
+            label="Azure endpoint",
+            value="https://open-ai-resource-gen-ai.openai.azure.com/",
+            type="password",
+            disabled=True
+        )
+        openai_api_version_val = st.text_input(
+            label="OpenAI API version",
+            value="2023-07-01-preview",
+            disabled=True
+        )
+        openai_api_key_val = st.text_input(
+            label="OpenAI API key",
+            placeholder="<api-key>",
+            type="password",
+            help="You can find your API key in the Azure or contact the Generative AI team."
+        )
 
-        slider_val = st.slider("Temperature", 0.0, 1.0, 0.1)
+        if openai_api_key_val == "<api-key>" or openai_api_key_val == "":
+            st.warning("Please enter your OpenAI API key.")
+
+        openai_api_type_val = st.text_input(
+            label="OpenAI API type",
+            value="azure",
+            disabled=True
+        )
+        deployment_name_val = st.text_input(
+            label="Deployment name",
+            value="gpt-35-dev",
+            disabled=True
+        )
+        model_name_val = st.text_input(
+            label="Model name",
+            value="gpt-35-turbo",
+            disabled=True
+        )
+        model_version_val = st.text_input(
+            label="Model version",
+            value="0613",
+            disabled=True
+        )
+        temperature_slider_val = st.slider(
+            label="Temperature",
+            min_value=0.0,
+            max_value=1.0,
+            value=0.5,
+            step=0.1,
+            help="A higher temperature value typically makes the output more diverse and creative but might also "
+                 "increase its likelihood of straying from the context. Conversely, a lower temperature value makes "
+                 "the AI's responses more focused and deterministic, sticking closely to the most likely prediction."
+        )
 
         # Submit button
-        submitted = st.form_submit_button(label="Save", on_click=st.balloons)
+        submitted = st.form_submit_button(label="Save ✅", on_click=st.balloons, use_container_width=True)
+
         if submitted:
             st.write("Model parameters saved with the following values:")
-            st.write("Temperature", slider_val)
+            st.write("Temperature", temperature_slider_val)
 
 elif st.session_state["authentication_status"] is False:
     # Display an error message if the user enters the wrong credentials
